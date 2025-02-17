97d58e8b62e2271feb2cbec11d052760cb1677c8
unknown <bhoure21@gmail.com>
2025-02-17 16:21:44 +0530

    commit changes

--- React/100xCohort/src/App.jsx
+++ React/100xCohort/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import Avatar from '../public/avatar.avif';
+import { ProjectList } from './data/projectList'
 
-const AppTodo = lazy(() => import('./to-do/AppTodo'))
+const AppTodo = lazy(() => import('../to-do/AppTodo'))
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path='/' element={<HomeScreen />} />
-          <Route path="/to-do" element={<AppTodo />} />
+          <Route path="/to-do/*" element={<AppTodo />} />
         </Routes>
       </Suspense>
     </div>
@@ -22,30 +23,44 @@ function App() {
 function HomeScreen() {
 
   return (
-    <div className='text-center text-white'>
-      <h1 className='text-4xl font-bold'>Practice React App</h1>
-      <div>
+    <div className='text-center text-white p-16'>
+      <h1 className='text-4xl font-bold mb-16'>Practice React App</h1>
+      <div className='max-w-max mx-auto grid gap-6 sm:grid-cols-2 md:grid-cols-3'>
+        {
+          ProjectList.length >= 1 ? ProjectList.map((project, index) => {
+            return (
+              <Card
+                key={index}
+                title={project.title}
+                description={project.description}
+                link={project.link}
+              />
+            )
+          }) :
+            <div> No Projects Found </div>
+        }
 
       </div>
-      <Card />
     </div>
   )
 }
 
-function Card() {
+function Card({ title, description, link }) {
 
   return (
-    <div className='flex-row w-60 bg-neutral-700 overflow-hidden rounded-lg'>
+    <div className='flex-row w-70 bg-neutral-700 overflow-hidden rounded-lg'>
       <img src={Avatar} alt='placeholder' />
-      <div className='flex justify-between'>
-        <div className='text-left'>
-          <h1 className='text-lg'>Title</h1>
-          <p className='text-sm'>description</p>
+      <div className='flex justify-between items-center bg-[#22222e] py-2 px-4'>
+        <div className='text-left '>
+          <h1 className='text-lg'>{title}</h1>
+          <p className='text-sm'>{description}</p>
         </div>
-        <button className='bg-[#7033ff]'> Open </button>
+        <NavLink to={link}>
+          <button className='bg-[#7033ff] py-2 px-4 rounded-lg hover:pointer'> Open </button>
+        </NavLink>
       </div>
-
     </div>
   )
 }
-export default App
+
+export default App;
---
+++ React/100xCohort/src/data/projectList.js
@@ -0,0 +1,7 @@
+export const ProjectList = [
+    {
+      title: 'To do app',
+      description: 'mern app',
+      link: '/to-do'
+    },
+  ]
